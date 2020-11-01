cde8abc04e309236afb0cbefaa18f7e5846252c6
MiaoZhuoYang <miaozhuoyang699@163.com>
2020-11-01 17:22:21 +0800

    init

---
+++ src/main/java/com/mzy/jenkins/JenkinsApplication.java
@@ -0,0 +1,17 @@
+package com.mzy.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther mzy
+ * @date 2020/11/1 16:58
+ */
+@SpringBootApplication
+public class JenkinsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JenkinsApplication.class);
+    }
+}
---
+++ src/main/java/com/mzy/jenkins/demo/DemoController.java
@@ -0,0 +1,20 @@
+package com.mzy.jenkins.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mzy
+ * @date 2020/11/1 16:59
+ */
+@RestController
+@RequestMapping("/jenkins")
+public class DemoController {
+
+    @RequestMapping("/demo")
+    @ResponseBody
+    public String demo(){
+        return "jenkins部署成功";
+    }
+}
